cf1d4cf282cfb2a13a9f71df939ee73dc21b5c04
=alankar <=>
2018-05-21 15:27:19 +0530

    First commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author BRACT-Admin
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Hi this is simple program for git-hub
+        System.out.println("helloworld.HelloWorld.main()");
+    }
+    
+}
